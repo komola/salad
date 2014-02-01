16a87c05faeb4b7c33221c7545cd61a5d13c9c2f
Sebastian Hoitz <hoitz@komola.de>
2014-02-01 11:39:21 +0100

    Use compiled javascript source if available

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-require("coffee-script");
-module.exports = require("./src/server/salad");
+var fs = require("fs");
+var path = require("path");
+
+var root = path.join(__dirname, "lib/server/salad.js");
+
+if(fs.existsSync(root))
+{
+  module.exports = require(root);
+}
+else
+{
+  require("coffee-script");
+  module.exports = require("./src/server/salad");
+}
