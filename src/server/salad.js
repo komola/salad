161708a68eab2035def2068502627a4fafc347c4
decaffeinate <claas.lange@finanzcheck.de>
2018-01-13 19:21:52 +0100

    decaffeinate: Run post-processing cleanups on base.coffee and 30 other files

--- src/server/salad.js
+++ src/server/salad.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 global.Salad = {};
 global.App = global.App || {};
 global._ = require("lodash");
